796e91dc83cf9073eae8235f606eedcb1def068c
hungphan2001 <phandinhhungvp2001@gmail.com>
2023-12-13 17:45:58 +0000

    vince:some change from QPR1
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- device.mk
+++ device.mk
@@ -86,8 +86,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.1-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
